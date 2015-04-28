ee7505f52bf27e09bea0fcae4cc3797e03945979
Jon Thornburg <reliable2192@gmail.com>
2015-04-28 11:43:30 -0600

    Initial Commit

---
+++ Navigator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Navigator
+//
+//  Created by jonathan thornburg on 4/28/15.
+//  Copyright (c) 2015 jonathan thornburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
