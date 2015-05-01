f585ce9e0ccb885316fed5603b2c97aa38bf2d52
Jon Thornburg <reliable2192@gmail.com>
2015-04-30 23:13:31 -0600

    done

--- Navigator/ViewController.m
+++ Navigator/ViewController.m
@@ -16,8 +16,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(35.0, 100.0, 100.0, 44.0)];
+    button.backgroundColor = [UIColor greenColor];
+    
+    [button addTarget:self action:@selector(pushNewViewController:)forControlEvents:UIControlEventTouchUpInside];
+    
+    UIStepper *stepper = [[UIStepper alloc]initWithFrame:CGRectMake(200.0, 200.0, 100.0, 100.0)];
+    [self.view addSubview:stepper];
+    
+    [stepper addTarget:self action:@selector(stepperChanged:) forControlEvents:UIControlEventValueChanged];
+    stepper.stepValue = 10.0;
+    
+    [self.view addSubview:button];
+    
 }
+
+- (void) stepperChanged:(UIStepper *)sender
+{
+    NSLog(@"%@", @(sender.value));
+}
+
+
+
+
+- (void)pushNewViewController:(id)sender
+{
+    UIViewController *secondViewController = [[UIViewController alloc]init];
+    secondViewController.view.backgroundColor = [UIColor redColor];
+    secondViewController.title = @"Second";
+    
+    //UIBarButtonItem *button = [[UIBarButtonItem alloc]initWithTitle:@"Another target:<#(id)#> action:<#(SEL)#>]
+    
+    
+    [self.navigationController pushViewController:secondViewController animated:YES];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
